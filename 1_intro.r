2948f92723922938009fe8901eaab32f71f65e64
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2023-03-28 00:56:54 +0530

    Add files via upload

---
+++ 1_intro.r
@@ -0,0 +1,17 @@
+print("Welcome R programming language")
+5+6
+sum1 <- 56 +78
+print(sum1)
+demo <- "Hello R Programmer"
+
+# this is rstudio introduction
+demo1 <- 2+3
+demo2 <- 5+6
+print(demo1)
+print(demo2)
+x <- 1:10
+plot(x)
+?plot(x)
+a = 10
+A = 20
+
---
+++ 3_datatypes.r
@@ -0,0 +1,22 @@
+#datatypes in R
+# logical,numeric,integer, complex, character,raw
+#numeric <- 10, 12,1314,-24,-25,,12.35,-26.12
+#integer= 24L
+#complex = 5+2i
+#logical = TRUE, FALSE
+#character = 'a', "hello R", "FALSE",'26.8952','hello R 12'
+
+num <- 10.25
+class(num)
+typeof(num)
+
+int1 <- 12L
+class(int1)
+typeof(int1)
+
+comp <- 5+2i
+class(comp)
+typeof(comp)
+
+logi <- TRUE
+class(logi)
