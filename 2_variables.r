89bac9f3965920ce8e7286b754c87ec10227cfe8
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2023-03-28 22:55:12 +0530

    Update 2_variables.r

--- 2_variables.r
+++ 2_variables.r
@@ -1,1 +1,12 @@
 ## Variables in R programming 
+var_name = 10
+var.name = 12
+.var_name <- 16
+var5 = 20
+
+## Assignment of Variable
+var1 <- 10
+var2 = 20
+30 -> var3
+print(var1)
+cat(var1," ",var2)
