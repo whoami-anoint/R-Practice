7d730b2d55f0fbb01b5fbefda707b7d05fb461cb
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2023-03-29 00:28:18 +0530

    Add files via upload

---
+++ 5_operators.r
@@ -0,0 +1,29 @@
+## Operators in R 
+# Arithmetic Operator = + - * / %% %/%
+# Relational Operator =  < > == <= >= !=
+# Logical Operator
+# Assignment Operator
+
+# Arithmetic Operation
+a <- 7.5
+b <-2 
+
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a%%b) #reminder
+print(a%/%b) #quotient
+print(a^b) #power of
+
+# Relational Operation 
+a < 7.5
+b <-2
+
+print(a<b) #less than
+print(a>b) #greater than
+print(a==b) #equal to
+print(a<=b) #less than or equals to
+print(a>=b) #greater than or equals to
+print(a!=) #not equals to
+
